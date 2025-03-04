3a381f92da709fc7f8842cb8215c8719f8ed59ff
Priyanshu7129 <priyanshu7129@gmail.com>
2025-03-04 11:56:51 +0530

    Implemented UC3: GET request with path variable

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,9 +1,6 @@
 package com.example.demo.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/hello")
@@ -18,6 +15,12 @@ public class HelloController {
     // UC2: GET request with a query parameter
     @GetMapping("/query")
     public String sayHelloWithQuery(@RequestParam String name) {
+        return "Hello " + name + " from BridgeLabz";
+    }
+
+    // UC3: GET request with a path variable
+    @GetMapping("/param/{name}")
+    public String sayHelloWithPath(@PathVariable String name) {
         return "Hello " + name + " from BridgeLabz";
     }
 }
